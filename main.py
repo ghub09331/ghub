a8b462e1719721df87898acac4ebbeb315231bb3
ghub09331 <ghub09331@gmail.com>
2023-05-23 15:50:06 +0900

    update

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import threading
 import requests
-import cfscrape
 import uuid
 import random
 import string
+import time
+import base64
 
 def randomstr(n):
    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
 
-roomId = input("roomId: ")
-nickname = input("nickname(%rand%): ")
-url = requests.get("https://garticphone.com/api/server?code="+roomId).text
-print(url)
+key = os.environ['key']
+global config
+config = {"url":""}
+global url
+url = ""
+global roomId
+roomId = ""
+global nickname
+nickname = "ニックネーム%rand%"
+
+def updater():
+    global url
+    global nickname
+    while True:
+        try:
+            roomId = base64.b64decode(requests.get("https://api.github.com/repos/ghub09331/ghub/contents/room.id",headers={"Accept": "application/vnd.github+json", "Authorization": "Bearer "+key}).json()["content"].encode()).decode()
+            url = requests.get("https://garticphone.com/api/server?code="+roomId).text
+        except:pass
 
 def joinbot():
     while True:
+        if url == "":time.sleep(5);continue
         try:
             t = randomstr(7);
             s = requests.session()
-            port = random.randint(10000,10049)
-            proxy = "socks5://127.0.0.1:"+str(port)
-            proxies = {"http":proxy,"https":proxy}
-            s.proxies = proxies
             sid = s.get(url+"/socket.io/?EIO=3&transport=polling&t="+t).text.split('{"sid":"')[1].split('"')[0]
             print(sid)
             uuid_ = str(uuid.uuid4())
@@ -36,5 +48,5 @@ def joinbot():
         except:pass
 #        except Exception as e:print(e)
 
-for _ in range(100):
+for _ in range(10):
     threading.Thread(target=joinbot).start()

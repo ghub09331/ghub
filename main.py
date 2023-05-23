7deaebaff1e5ccea593c2086cf50640d81ebe7ec
ghub09331 <ghub09331@gmail.com>
2023-05-23 15:52:17 +0900

    update

--- main.py
+++ main.py
@@ -36,14 +36,11 @@ def joinbot():
             t = randomstr(7);
             s = requests.session()
             sid = s.get(url+"/socket.io/?EIO=3&transport=polling&t="+t).text.split('{"sid":"')[1].split('"')[0]
-            print(sid)
             uuid_ = str(uuid.uuid4())
-            print(uuid_)
             skin = str(random.randint(0,45))
             nick = nickname.replace("%rand%",str(random.randint(100,9999)))
             body = str('[1,"'+uuid_+'","'+nick+'",'+skin+',"ja",false,"'+roomId+'",null,null]')
             body = str(str(len(body)+2)+":42"+body).encode("utf-8")
-            print(body)
             res = s.post(url+"/socket.io/?EIO=3&transport=polling&t="+t+"&sid="+sid, headers={"Content-Type":"text/plain;charset=UTF-8"}, data=body).text
             print(res)
         except:pass
